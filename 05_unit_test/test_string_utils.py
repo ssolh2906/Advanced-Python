b8bd5d2ec96dad4212159f4f700775c1a35aa730
Sol_P_T <91825053+ssolh2906@users.noreply.github.com>
2026-05-11 14:22:57 -0700

    Test reverse string

--- 05_unit_test/test_string_utils.py
+++ 05_unit_test/test_string_utils.py
@@ -5,8 +5,36 @@ Include tests for both valid and invalid inputs, edge cases, and expected behavi
 Organize tests logically within the class.
 """
 
+from unittest import TestCase
+from string_utils import reverse_string, is_palindrome
+
+
+class TestStringUtils(TestCase):
+
+    def test_reverse_string_valid(self):
+        self.assertEqual(reverse_string("my string"), "gnirts ym")
+        self.assertEqual(reverse_string(""), "")
+
+    def test_reverse_string_invalid(self):
+        self.assertRaises(TypeError, reverse_string, 123)
+        self.assertRaises(TypeError, reverse_string, ["Invalide input"])
+        self.assertRaises(TypeError, reverse_string, None)
+
+    def test_reverse_string_edge_cases(self):
+        self.assertEqual(reverse_string(""), "")
+        self.assertEqual(reverse_string("a"), "a")
+        self.assertEqual(reverse_string("12345"), "54321")
+
+    def test_reverse_string_non_string_input(self):
+        self.assertRaises(TypeError, reverse_string, 1)
+
+
 """
 3. Run the tests: Execute the tests using python -m unittest test_string_utils.py.
 Observe the test results and ensure all tests pass.
 """
 
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
