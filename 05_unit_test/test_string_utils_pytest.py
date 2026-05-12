1ff255dbdc5d39207541d67a2beef7561cd278e0
Solhee Tucker <solheepark29@gmail.comsolheepark29@gmail.comsolheepark29@gmail.com>
2026-05-12 16:25:49 -0700

    pytest

--- 05_unit_test/test_string_utils_pytest.py
+++ 05_unit_test/test_string_utils_pytest.py
@@ -8,7 +8,32 @@ import pytest
 from string_utils import reverse_string, is_palindrome
 
 class TestStringUtilsPytest:
-    def test_reverse_string_valid(self):
+
+    @pytest.mark.parametrize("input_string, expected_output", [
+        ("hello", "olleh"),
+        ("tacocat", "tacocat"),
+        ("", ""),
+        ("a", "a"),
+    ])
+    def test_reverse_string_valid(self, input_string, expected_output):
+        assert reverse_string(input_string) == expected_output
+
+    def test_reverse_string_exception(self):
+        with pytest.raises(TypeError):
+            reverse_string(123)
+
+    @pytest.mark.parametrize("input_string, expected_output", [
+        ("hello", False),
+        ("tacocat", True),
+        ("", True),
+        ("a", True),
+    ])
+    def test_is_palindrome_valid(self, input_string, expected_output):
+        assert is_palindrome(reverse_string(input_string)) == expected_output
+
+    def test_is_palindrome_exception(self):
+        with pytest.raises(TypeError):
+            is_palindrome(123)
 
 
 """
